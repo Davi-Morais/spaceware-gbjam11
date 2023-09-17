3fecfb8f2959a033711e8dc0fa7d1767cf6d06b3
Davi Morais <davifarias2433@gmail.com>
2023-09-16 22:14:34 -0300

    fanzendo animacao do botao no topo do minigame

--- miniGameFoguete.lua
+++ miniGameFoguete.lua
@@ -16,6 +16,12 @@ function miniGameFoguete:new()
 
     self.cenario = love.graphics.newImage("assets/miniGameFoguete/cenario.png")
 
+    self.botoes = {}
+    self.botoes.botao1 = love.graphics.newImage("assets/miniGameFoguete/botao1.png")
+    self.botoes.botao2 = love.graphics.newImage("assets/miniGameFoguete/botao2.png")
+    self.botoes.qual = 2 --Qual botao vai ser desenhado
+    self.botoes.intervalo = 0.3
+    self.botoes.temporizador = 0
 
     return miniGameFoguete
 end
@@ -23,12 +29,30 @@ end
 
 function miniGameFoguete:update(dt)
     moverNuvens(self.nuvens, dt)
-
+    buttonAnimation(self.botoes, dt)
 end
 
 function moverNuvens(nuvens, dt)
     nuvens.x = nuvens.x + nuvens.speed * dt
 end
+
+
+function buttonAnimation(botoes, dt)
+    botoes.temporizador = botoes.temporizador + dt
+
+    if botoes.temporizador >= botoes.intervalo then 
+        if botoes.qual == 1 then
+            botoes.qual = 2
+        else
+            botoes.qual = 1
+        end
+
+        botoes.temporizador = botoes.temporizador - botoes.intervalo
+
+    end
+end
+
+
 
 
 function miniGameFoguete:draw()
@@ -37,5 +61,11 @@ function miniGameFoguete:draw()
     love.graphics.draw(self.nuvens.image, self.nuvens.x, self.nuvens.y)
 
     love.graphics.draw(self.cenario, 0, 0)
+
+    if self.botoes.qual == 1 then
+        love.graphics.draw(self.botoes.botao1, 0, 0)
+    else
+        love.graphics.draw(self.botoes.botao2, 0, 0)
+    end
 
 end
