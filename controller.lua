4f17e8b2ea89e929377cf35a01616a5aa9ab0a74
Davi Morais <davifarias2433@gmail.com>
2023-09-18 11:07:48 -0300

    Iniciando logica do controller

--- controller.lua
+++ controller.lua
@@ -1,3 +1,5 @@
+require("miniGameFoguete")
+
 controller = {}
 
 function controller:new()
@@ -5,17 +7,41 @@ function controller:new()
     setmetatable(controller, self)
     self.__index = self
 
+    self.miniGames = {}
+    self.miniGames.foguete = miniGameFoguete:new()
+
+    self.timer = 0
+    self.current = nil
+
     return controller
 end
 
 
 function controller:update(dt)
-    
+    if self.current then
+
+        self.current:update(dt)
+
+        if self.current.venceu == true or self.current.venceu == false then
+            self.current:resetarMiniGame()
+            self.current = nil
+        end
+        return
+    end
+
+    self.timer = self.timer + dt
+
+    if self.timer >= 5 then
+        print('5 segundos passaram!')
+        self.current = self.miniGames.foguete
+        self.timer = 0
+    end
 end
 
 
 function controller:draw()
-    love.graphics.setColor(1, 0, 0)
-    love.graphics.circle("fill", 100, 100, 15, 5)
+    if self.current then
+        self.current:draw()
+    end
 end
  
